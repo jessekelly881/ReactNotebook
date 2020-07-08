a42169d457584a16b647fdc04c4fbe3faed4df7f
Jesse Kelly <kellyjesse881@gmail.com>
2020-07-08 10:21:35 -0500

    feat(ui): Styled preview area

--- src/ui/index.js
+++ src/ui/index.js
@@ -4,12 +4,21 @@ import styled, { css } from "styled-components";
 import React from "react";
 import "./index.scss";
 
+const StyledPreview = styled(LivePreview)`
+    position: relative;
+    padding: 0.5rem;
+    background: #fafafa;
+    color: black;
+    height: auto;
+    overflow: hidden;
+`;
+
 export default () => (
     <section id="app">
         <LiveProvider code="<strong>Hello World!</strong>">
             <LiveEditor />
             <LiveError />
-            <LivePreview />
+            <StyledPreview />
         </LiveProvider>
     </section>
 );

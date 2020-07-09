f643cedcdcc7869cb6b2ae6244b58a7883215655
Jesse Kelly <kellyjesse881@gmail.com>
2020-07-09 16:12:09 -0500

    feat(ui): added support me text

--- src/ui/index.js
+++ src/ui/index.js
@@ -4,6 +4,17 @@ import AppRouter from "./router";
 /**
  * App
  */
-const App = props => <AppRouter />;
+const App = props => (
+    <>
+        <AppRouter />
+        <div style={{ textAlign: "center", margin: "2rem" }}>
+            <small style={{ color: "#ccc" }}>
+                Support my work. With bitcoinz!
+                <br />
+                bc1q60twsepwkjpf8w7myaupkr2fxkuc0cze0wxygh
+            </small>
+        </div>
+    </>
+);
 
 export default App;

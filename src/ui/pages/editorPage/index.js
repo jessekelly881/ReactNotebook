6162b506e9772f75c103c42e23655930ca7f5f8a
Jesse Kelly <kellyjesse881@gmail.com>
2020-08-18 13:08:15 -0500

    fix(ui): fixed excessive getNotebook calls

--- src/ui/pages/editorPage/index.js
+++ src/ui/pages/editorPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getNotebook, updateNotebook } from "services/firebase";
 import FullEditor from "ui/components/editor";
@@ -7,10 +7,11 @@ import FullEditor from "ui/components/editor";
  * EditorController
  */
 export const EditorController = ({ dbValue, id, ...props }) => {
-    const updateValue = val => {
+    const updateValue = useCallback(val => {
         setValue(val);
+        alert("Update notebook");
         updateNotebook(id)({ data: val });
-    };
+    });
 
     const [value, setValue] = useState(dbValue);
     return <FullEditor value={value} setValue={updateValue} {...props} />;
@@ -23,14 +24,16 @@ const EditorPage = props => {
     const { id } = useParams();
     const [editorValue, setEditorValue] = useState();
 
-    getNotebook(id).then(doc => {
-        if (doc.exists) {
-            setEditorValue(doc.data().data);
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
-    });
+    useEffect(_ => {
+        getNotebook(id).then(doc => {
+            if (doc.exists) {
+                setEditorValue(doc.data().data);
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        });
+    }, []);
 
     if (editorValue) {
         return <EditorController id={id} dbValue={editorValue} />;
